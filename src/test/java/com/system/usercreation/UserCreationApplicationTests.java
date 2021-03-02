b561eb362ab0d9577150ebd21253fb01a2b841d5
p2gupta <p2gupta@bravurasolutions.com>
2021-03-02 22:53:10 +0530

    Initial Project Commit

---
+++ src/test/java/com/system/usercreation/UserCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.usercreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
